9b72d353a0b36a4e9d3ccf37bcc8a35d80b257d3
Theodore Roumeliotis <thodorisroum@hotmail.com>
2025-03-25 17:07:33 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/AddApp.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.ch1;
+
+public class AddApp {
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 15;
+        int result = 0;
+
+        result = num1 + num2;
+
+        System.out.println("Το άθροισμα του " + num1 + " και " + num2  + " ειναι " + result);
+
+    }
+}
